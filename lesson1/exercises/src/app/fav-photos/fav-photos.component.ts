e4c97d56aa5a98de9bdc66cd3b7a86fc4ee8bf27
kgeiger08 <72526597+kgeiger08@users.noreply.github.com>
2020-11-02 15:46:31 -0600

    Added code to exercise initial commit

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://images.pexels.com/photos/407082/dog-face-labrador-smile-407082.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
+  image3 = 'https://images.pexels.com/photos/3663082/pexels-photo-3663082.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500';
 
   constructor() { }
 
